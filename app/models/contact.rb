42f10528c5ab2c05e7654840064040731998776f
“leonshimizu” <lmshimizu@gmail.com>
2021-10-20 16:59:19 -0700

    corrected phone number format

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  def phone_number_format
+    return "(#{phone_number[0]}#{phone_number[1]}#{phone_number[2]})#{phone_number[3]}#{phone_number[4]}#{phone_number[5]}-#{phone_number[6]}#{phone_number[7]}#{phone_number[8]}#{phone_number[9]}"
+  end
 end
